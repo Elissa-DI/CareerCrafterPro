8c917bf170399bfbef1ae870878f1cd163c21d6e
Elissa <elissadusabe@gmail.com>
2023-08-07 03:45:03 +0200

    new features

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { StyleSheet, View,Text } from 'react-native';
+
+export default function App() {
+  return(
+    <View>
+      <Text>Hello this  bull the baddesr</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+        <Text>Home page</Text>
+    </View>
+  )
+}
+
+export default Home
